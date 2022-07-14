e39f4b6c98fc58ae84f13e0997ad868fb4d87728
z to zee h <zhoomart.d@gmail.com>
2022-07-14 17:49:11 +0600

    refactor: cartBtn.js

--- src/components/CartBtn/CartBtn.js
+++ src/components/CartBtn/CartBtn.js
@@ -19,7 +19,7 @@ const CartBtn = (props) => {
 
     const checkProductId = () => {
         const backup = {
-            "id": 101,
+            "id": 9,
             "img": "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
             "desc": "Булка, котлета, сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус, гриль, кетчуп, майонез",
             "price": 200,
@@ -27,7 +27,7 @@ const CartBtn = (props) => {
         }
         let cartData = JSON.parse(localStorage.getItem('cart'));
 
-        if (cartData === null){
+        if (cartData === undefined){
             cartData = localStorage.setItem('cart', JSON.stringify(backup))
         } else {
             cartData = Object.values(cartData);
