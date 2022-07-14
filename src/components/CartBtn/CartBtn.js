259ea1c846787291b2222700130bd4699d14fef8
z to zee h <zhoomart.d@gmail.com>
2022-07-14 16:58:41 +0600

    refactor: check Object.values

--- src/components/CartBtn/CartBtn.js
+++ src/components/CartBtn/CartBtn.js
@@ -20,16 +20,7 @@ const CartBtn = (props) => {
     const checkProductId = () => {
         let cartData = JSON.parse(localStorage.getItem('cart'));
         cartData = Object.values(cartData);
-        if (cartData !== null){
-            setProductsFromStorage(cartData);
-        } else{
-            cartData = localStorage.setItem('cart', JSON.stringify({
-                "id": 101,
-                "img": "https://images.unsplash.com/photo-1572294843899-535506e8bb42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=808&q=80",
-                "desc": "default",
-                "title": "placeholder"
-            }))
-        }
+        setProductsFromStorage(cartData);
 
         let result = cartData.find(item => item.id === props.id);
         if (result){
