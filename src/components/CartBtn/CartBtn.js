fecb47d86d98e78a026d4ce27d7ac9b1451a2a14
z to zee h <zhoomart.d@gmail.com>
2022-07-14 18:18:30 +0600

    refactor: cartBtn.js

--- src/components/CartBtn/CartBtn.js
+++ src/components/CartBtn/CartBtn.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import styles from "./CartBtn.module.css";
+import {BASE_URL} from "../../constants";
+import toast from "react-hot-toast";
 
 const CartBtn = (props) => {
 
@@ -18,17 +20,15 @@ const CartBtn = (props) => {
     }
 
     const checkProductId = () => {
-        const backup = {
-            "id": 9,
-            "img": "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-            "desc": "Булка, котлета, сыр, соленый огурец, лук, помидор, салат айсберг, соус чесночный, соус, гриль, кетчуп, майонез",
-            "price": 200,
-            "title": "Чизбургер"
-        }
+
         let cartData = JSON.parse(localStorage.getItem('cart'));
 
         if (cartData === undefined){
-            cartData = localStorage.setItem('cart', JSON.stringify(backup))
+            const backup = 'https://my-json-server.typicode.com/JayDeeJS/food-deploy-server/backUpProduct';
+            fetch(backup)
+                .then(response => response.json())
+                .then(data => data);
+            cartData = localStorage.setItem('cart', JSON.stringify('backup'))
         } else {
             cartData = Object.values(cartData);
             setProductsFromStorage(cartData);
